8becf0ed51465cde5c7bebf1e83bd440b6684a1b
Saurav kumar verma <sauravkumarverma22@gmail.com>
2023-08-20 21:06:18 +0530

    Project Completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,3 +29,35 @@ const toggleNavbar = () => {
 
 // Making the Toggle Work
 addEventOnElements([navToggleBtn, overlay], "click", toggleNavbar);
+
+/**
+ *** *** Parallax Effect
+ */
+
+const parallaxElements = document.querySelectorAll("[data-parallax]");
+
+window.addEventListener("mousemove", (event) => {
+  for (let i = 0; i < parallaxElements.length; i++) {
+    const movementX =
+      (event.clientX / window.innerWidth) *
+      Number(parallaxElements[i].dataset.parallaxSpeed);
+
+    // console.log("a ", event.clientX);
+    // console.log("b ", window.innerWidth);
+    // console.log("c ", parallaxElements[i].dataset.parallaxSpeed);
+
+    // console.log(movementX);
+    const movementY =
+      (event.clientY / window.innerHeight) *
+      Number(parallaxElements[i].dataset.parallaxSpeed);
+
+    // console.log(movementY);
+
+    parallaxElements[i].animate(
+      {
+        transform: `translate(${movementX}px, ${movementY}px)`,
+      },
+      { duration: 500, fill: "forwards" }
+    );
+  }
+});
